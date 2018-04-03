95b0dbb948a8b2febf2c88d12a54626f8d33b9d5
LilyGO <31160602+LilyGO@users.noreply.github.com>
2018-04-03 09:38:34 +0800

    First commit

---
+++ ShutDown/ShutDown.ino
@@ -0,0 +1,22 @@
+#include <Keyboard.h>
+void setup() {//init
+  Keyboard.begin();//Start keyboard communication 
+  delay(3000);//Delay
+  Keyboard.press(KEY_LEFT_GUI);//win Button
+  delay(500); 
+  Keyboard.press('r');//R Button//Win+R
+  delay(500); 
+  Keyboard.release(KEY_LEFT_GUI);
+  Keyboard.press(KEY_RETURN);//enter Button
+  delay(500);
+  Keyboard.release(KEY_RETURN);
+  delay(500); 
+  Keyboard.println("shutdown -s -f -t 0"); //Shutdown
+  Keyboard.press(KEY_RETURN);
+  delay(500);
+  Keyboard.release(KEY_RETURN);
+  Keyboard.end();//结束键盘通讯 
+}
+void loop()//循环
+{
+}
